a1de7cf66dde5d59b2f8ac52fb5fa106e66cb859
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2024-09-12 17:24:44 +0800

    Update cfopw.sh

--- cfopw.sh
+++ cfopw.sh
@@ -14,11 +14,10 @@ CFOPW_SCRIPT_LOCAL="cfopw.sh"
 
 # 检测 setup_cloudflarest.sh 是否有更新
 echo "正在检查 setup_cloudflarest.sh 是否有更新..."
-curl -s -z "$SETUP_SCRIPT_LOCAL" -o "$SETUP_SCRIPT_LOCAL" "$SETUP_SCRIPT_URL"
-if [ $? -eq 0 ]; then
-    echo "setup_cloudflarest.sh 已经是最新版本。"
+if curl -s -z "$SETUP_SCRIPT_LOCAL" -o "$SETUP_SCRIPT_LOCAL" "$SETUP_SCRIPT_URL"; then
+  echo "setup_cloudflarest.sh 已经是最新版本。"
 else
-    echo "已更新 setup_cloudflarest.sh。"
+  echo "已更新 setup_cloudflarest.sh。"
 fi
 
 # 下载 resolve_cloudflare.sh 并赋予执行权限
@@ -28,21 +27,14 @@ chmod +x "$RESOLVE_SCRIPT_LOCAL"
 
 # 提示用户 resolve_cloudflare.sh 已经准备好，可以执行
 echo "resolve_cloudflare.sh 已经下载完成并赋予执行权限。"
-echo "你可以通过 './$RESOLVE_SCRIPT_LOCAL' 来执行解析。"
 
-# 检测并更新 cfopw.sh
-echo "正在检查 cfopw.sh 是否有更新..."
-curl -s -z "$CFOPW_SCRIPT_LOCAL" -o "$CFOPW_SCRIPT_LOCAL" "$CFOPW_SCRIPT_URL"
-if [ $? -eq 0 ]; then
-    echo "cfopw.sh 已经是最新版本。"
+# 提示用户 cfopw.sh 自身已经更新
+if curl -s -z "$CFOPW_SCRIPT_LOCAL" -o "$CFOPW_SCRIPT_LOCAL" "$CFOPW_SCRIPT_URL"; then
+  echo "cfopw.sh 已经是最新版本。"
 else
-    echo "cfopw.sh 已更新。"
+  echo "cfopw.sh 已更新。"
 fi
 
 # 执行 resolve_cloudflare.sh
 echo "正在执行 resolve_cloudflare.sh..."
-if [ -x "$RESOLVE_SCRIPT_LOCAL" ]; then
-    ./"$RESOLVE_SCRIPT_LOCAL"
-else
-    echo "无法执行 resolve_cloudflare.sh，文件不可执行。"
-fi
+./"$RESOLVE_SCRIPT_LOCAL"
