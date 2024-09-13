00cbfa2a3a45876f2aec4f3d46f6618f1fe955bf
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2024-09-13 22:49:31 +0800

    Update cfopw.sh

--- cfopw.sh
+++ cfopw.sh
@@ -14,8 +14,13 @@ CFOPW_SCRIPT_URL="https://mirror.ghproxy.com/https://raw.githubusercontent.com/G
 echo "正在删除旧的脚本文件..."
 rm -f "$SETUP_SCRIPT_LOCAL" "$RESOLVE_SCRIPT_LOCAL" "$CFOPW_SCRIPT_LOCAL"
 
-# 下载并更新 setup_cloudflarest.sh 并赋予执行权限
-echo "检查并更新 setup_cloudflarest.sh..."
+# 下载 setup_cloudflarest.sh 并赋予执行权限
+MAX_RETRIES=3
+RETRY_DELAY=5
+retry_count=0
+
+while [ $retry_count -lt $MAX_RETRIES ]; do
+echo "获取 setup_cloudflarest.sh..."
 if curl -ksSL -o "$SETUP_SCRIPT_LOCAL" "$SETUP_SCRIPT_URL"; then
   echo "setup_cloudflarest.sh 已更新。"
   chmod +x "$SETUP_SCRIPT_LOCAL"
@@ -24,8 +29,13 @@ else
   exit 1
 fi
 
-# 下载并更新 cf.sh 并赋予执行权限
-echo "检查并更新 cf.sh..."
+# 下载 cf.sh 并赋予执行权限
+echo "获取 cf.sh..."
+MAX_RETRIES=3
+RETRY_DELAY=5
+retry_count=0
+
+while [ $retry_count -lt $MAX_RETRIES ]; do
 if curl -ksSL -o "$RESOLVE_SCRIPT_LOCAL" "$RESOLVE_SCRIPT_URL"; then
   echo "cf.sh 已更新。"
   chmod +x "$RESOLVE_SCRIPT_LOCAL"
@@ -34,8 +44,13 @@ else
   exit 1
 fi
 
-# 下载并更新 cfopw.sh
-echo "检查并更新 cfopw.sh..."
+# 下载 cfopw.sh
+MAX_RETRIES=3
+RETRY_DELAY=5
+retry_count=0
+
+while [ $retry_count -lt $MAX_RETRIES ]; do
+echo "获取 cfopw.sh..."
 if curl -ksSL -o "$CFOPW_SCRIPT_LOCAL" "$CFOPW_SCRIPT_URL"; then
   echo "cfopw.sh 已更新。"
 else
