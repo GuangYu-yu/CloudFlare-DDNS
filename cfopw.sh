7ce663bbbe80a4fc247a3be9b2d9400131be8e3d
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2025-05-24 17:10:48 +0800

    Update cfopw.sh

--- cfopw.sh
+++ cfopw.sh
@@ -41,6 +41,73 @@ download_script() {
     done
 }
 
+# 定义执行脚本的函数
+execute_script() {
+    local script_local=$1
+    local script_name=$2
+
+    echo "正在执行 $script_name..."
+    if ./"$script_local"; then
+        echo "$script_name 执行完成"
+    else
+        echo "执行 $script_name 失败"
+        exit 1
+    fi
+}
+
+# 初始化包列表
+packages=""
+
+# 检查依赖是否安装
+check_dependency() {
+    local dependency=$1
+    local package_name=$2
+
+    if ! command -v "$dependency" &> /dev/null; then
+        echo "$dependency 未找到，将其添加到所需软件包列表中"
+        packages="$packages $package_name"
+    else
+        echo "$dependency 已安装"
+    fi
+}
+
+# 检查所有依赖
+check_dependency "jq" "jq"
+check_dependency "yq" "yq"
+check_dependency "tar" "tar"
+check_dependency "sed" "sed"
+check_dependency "awk" "gawk"
+
+check_dependency "tr" "coreutils-tr"
+check_dependency "timeout" "coreutils-timeout"
+check_dependency "base64" "coreutils-base64"
+
+# 判断系统进行安装
+if [ -n "$packages" ]; then
+    echo "以下软件包是必需的: $packages"
+    if grep -qi "alpine" /etc/os-release; then
+        echo "使用 apk 安装软件包..."
+        apk update
+        apk add $packages
+    elif grep -qi "openwrt" /etc/os-release; then
+        echo "使用 opkg 安装软件包..."
+        opkg update
+        for package in $packages; do
+            opkg install "$package"
+        done
+    elif grep -qi "ubuntu\|debian" /etc/os-release; then
+        echo "使用 apt-get 安装软件包..."
+        sudo apt-get update
+        sudo apt-get install $packages
+    elif grep -qi "centos\|red hat\|fedora" /etc/os-release; then
+        echo "使用 yum 安装软件包..."
+        sudo yum install $packages
+    else
+        echo "未能检测出你的系统：$(uname)，请自行安装 $packages"
+        exit 1
+    fi
+fi
+
 # 下载所有脚本
 download_script "$RESOLVE_SCRIPT_LOCAL" "$RESOLVE_SCRIPT_API" "cf"
 download_script "$START_DDNS_LOCAL" "$START_DDNS_API" "start_ddns.sh"
@@ -48,35 +115,3 @@ download_script "$CF_PUSH_LOCAL" "$CF_PUSH_API" "cf_push.sh"
 
 # 执行 setup_cloudflarest.sh
 curl -ksSL https://gitee.com/zhxdcyy/sh/raw/master/setup_cloudflarest.sh | bash
-
-# 初始化包列表
-packages=""
-
-# 检查依赖函数
-check_dependency() {
-    local dependency=$1
-    local package_name=$2
-    if ! command -v "$dependency" >/dev/null 2>&1; then
-        echo "$dependency 未找到，将其添加到待安装软件包列表中"
-        packages="$packages $package_name"
-    else
-        echo "$dependency 已安装"
-    fi
-}
-
-# 检查依赖
-check_dependency "jq" "jq"
-check_dependency "yq" "yq"
-check_dependency "tar" "tar"
-check_dependency "sed" "sed"
-check_dependency "awk" "gawk"
-check_dependency "tr" "coreutils"
-
-# 检查 timeout 和 base64 是否可用
-check_dependency "timeout" "coreutils-timeout"
-check_dependency "base64" "coreutils-base64"
-
-# 自动检测包管理器进行安装
-if [ -n "$packages" ]; then
-    echo "需要安装以下软件包: $packages"
-fi
