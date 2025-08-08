48deb5eab891588328956a956546c3af5d1587f9
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2025-08-08 12:37:56 +0800

    Update cfopw.sh

--- setup/cfopw.sh
+++ setup/cfopw.sh
@@ -1,59 +1,58 @@
 #!/bin/bash
 
-# 定义文件路径和API URL
+# 获取当前系统架构
+ARCH=$(uname -m)
 
-RESOLVE_SCRIPT_LOCAL="cf"
-RESOLVE_SCRIPT_API="https://gitee.com/zhxdcyy/sh/raw/master/cf"
+# 根据系统架构选择相应的文件名
+case "$ARCH" in
+    x86_64) 
+        FILENAME="CFRS_linux_amd64.tar.gz"
+        DOWNLOAD_URL="https://github.com/GuangYu-yu/CloudFlare-DDNS/releases/download/latest/CFRS_linux_amd64.tar.gz"
+        ;;
+    aarch64) 
+        FILENAME="CFRS_linux_arm64.tar.gz"
+        DOWNLOAD_URL="https://github.com/GuangYu-yu/CloudFlare-DDNS/releases/download/latest/CFRS_linux_arm64.tar.gz"
+        ;;
+    *)
+        echo "不支持的架构: $ARCH"
+        exit 1 ;;
+esac
 
-START_DDNS_LOCAL="start_ddns.sh"
-START_DDNS_API="https://gitee.com/zhxdcyy/sh/raw/master/start_ddns.sh"
+# 如果文件存在，则删除
+if [ -f "$FILENAME" ]; then
+    echo "$FILENAME 已存在，删除旧文件..."
+    rm -f "$FILENAME"
+fi
 
-CF_PUSH_LOCAL="cf_push.sh"
-CF_PUSH_API="https://gitee.com/zhxdcyy/sh/raw/master/cf_push.sh"
+# 定义重试次数
+MAX_RETRIES=3
+RETRY_COUNT=0
 
-# 定义下载脚本的函数
-download_script() {
-    local script_local=$1
-    local script_api=$2
-    local script_name=$3
-
-    MAX_RETRIES=3
-    RETRY_DELAY=5
-    retry_count=0
-
-    while [ $retry_count -lt $MAX_RETRIES ]; do
-        echo "获取 $script_name..."
-        if curl -sL "$script_api" -o "$script_local"; then
-            echo "$script_name 已更新"
-            chmod +x "$script_local"
-            break
-        else
-            echo "下载 $script_name 失败"
-            retry_count=$((retry_count + 1))
-            if [ $retry_count -lt $MAX_RETRIES ]; then
-                echo "将在 $RETRY_DELAY 秒后重试..."
-                sleep $RETRY_DELAY
-            else
-                echo "重试次数已达上限，退出"
-                exit 1
-            fi
-        fi
-    done
-}
-
-# 定义执行脚本的函数
-execute_script() {
-    local script_local=$1
-    local script_name=$2
-
-    echo "正在执行 $script_name..."
-    if ./"$script_local"; then
-        echo "$script_name 执行完成"
+# 下载
+echo "下载 $FILENAME"
+while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
+    if curl --max-time 10 -O "$DOWNLOAD_URL"; then
+        echo "下载成功！"
+        break
     else
-        echo "执行 $script_name 失败"
-        exit 1
+        RETRY_COUNT=$((RETRY_COUNT + 1))
+        echo "下载失败，重试第 $RETRY_COUNT 次..."
     fi
-}
+done
+
+# 清理临时文件
+rm -rf "$TEMP_DIR"
+
+# 解压
+tar -zxf "$FILENAME" && rm -f "$FILENAME"
+
+# 删除压缩包
+rm -f "$FILENAME"
+
+# 赋予执行权限
+chmod +x CFRS
+
+echo "CFRS 获取完成！"
 
 # 初始化包列表
 packages=""
@@ -72,15 +71,7 @@ check_dependency() {
 }
 
 # 检查所有依赖
-check_dependency "jq" "jq"
-check_dependency "yq" "yq"
 check_dependency "tar" "tar"
-check_dependency "sed" "sed"
-check_dependency "awk" "gawk"
-
-check_dependency "tr" "coreutils-tr"
-check_dependency "timeout" "coreutils-timeout"
-check_dependency "base64" "coreutils-base64"
 
 # 判断系统进行安装
 if [ -n "$packages" ]; then
@@ -108,10 +99,5 @@ if [ -n "$packages" ]; then
     fi
 fi
 
-# 下载所有脚本
-download_script "$RESOLVE_SCRIPT_LOCAL" "$RESOLVE_SCRIPT_API" "cf"
-download_script "$START_DDNS_LOCAL" "$START_DDNS_API" "start_ddns.sh"
-download_script "$CF_PUSH_LOCAL" "$CF_PUSH_API" "cf_push.sh"
-
 # 执行 setup_cloudflarest.sh
-curl -ksSL https://gitee.com/zhxdcyy/sh/raw/master/setup_cloudflarest.sh | bash
+curl -ksSL https://raw.githubusercontent.com/GuangYu-yu/CloudFlare-DDNS/refs/heads/main/setup/setup_cloudflarest.sh | bash
